fe5c54e40a27dca97dfc81df186ea63891f9f2da
Randel Anderson <randel.p.anderson@jsums.edu>
2015-03-23 12:08:16 -0500

    Working with data in app file

--- schedule/src/app.js
+++ schedule/src/app.js
@@ -1,3 +1,72 @@
-var termList = React.createClass({
-  
-})
+var terms=[
+    {"term_code":"201509","term_desc":"Fall 2015"},
+    {"term_code":"201501","term_desc":"Spring 2015"}
+]
+
+var Term = React.createClass({
+  render: function(){
+    return(
+      <li className="term">
+        <div className="termCode">
+          {this.props.data.term_code} - {this.props.data.term_desc}
+        </div>
+      </li>
+    );
+  }
+});
+
+var TermList = React.createClass({
+  render: function(){
+    return(
+      <div className="termList">
+        {this.props.terms.map(function(term){
+          return <Term key={term.term_code} data={term}/>;
+        })}
+      </div>
+    );
+  }
+});
+
+// Below is an example of another way to implement TermList
+/* var TermList = React.createClass({
+  render: function(){
+    var termNodes = this.props.terms.map(function(term){
+      return(
+        <Term key={term.term_code} termCode={term.term_code}>
+          {term.term_desc}
+        </Term>
+      );
+    });
+    return(
+      <div className="termList">
+        {termNodes}
+      </div>
+    );
+  }
+}); */
+
+var DeptList = React.createClass({
+  render: function(){
+    return(
+      <div className="deptList">
+        Hello, World! I am a DeptList.
+      </div>
+    );
+  }
+});
+
+var CourseList = React.createClass({
+  render: function(){
+    return(
+      <div className="courseList">
+        Hello, World! I am a CourseList.
+      </div>
+    );
+  }
+});
+
+
+React.render(
+  <TermList terms={terms} />,
+  document.getElementById('content')
+);
