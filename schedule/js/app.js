2af8775e2b6acdac1bc95267590d0c76fc8a9b3f
Randel Anderson <randel.p.anderson@jsums.edu>
2015-03-30 17:12:05 -0500

    Backbone Router version

---
+++ schedule/js/app.js
@@ -0,0 +1,31 @@
+
+var Router = Backbone.Router.extend({
+
+  routes: {
+    ':term/crn/:crn': 'sectionDetailRoute',
+    ':term': 'deptListRoute',
+    ':term/:dept': 'courseListRoute',
+    ':term/:dept/:cid': 'courseDetailRoute',
+    '*notFound': 'defaultRoute',
+    '': 'defaultRoute'
+  },
+  deptListRoute: function(term){
+    document.getElementById('rootContainer').innerHTML = '<a href="#/201509/ACC">' + term + '</a>';
+  },
+  courseListRoute: function(term, dept){
+    document.getElementById('rootContainer').innerHTML = '<a href="#/201509/ACC/211">' + term + dept + '</a>';
+  },
+  courseDetailRoute: function(term, dept, cid){
+    document.getElementById('rootContainer').innerHTML = '<a href="#/201509/crn/405145">' + term + dept + cid + '</a>';
+  },
+  sectionDetailRoute: function(term, crn){
+    document.getElementById('rootContainer').innerHTML = '<a href="#">Section' + term + crn + '</a>';
+  },
+  defaultRoute: function(){
+    document.getElementById('rootContainer').innerHTML = '<a href="#/201509">Select A Term</a>';
+  }
+
+});
+
+var appRouter = new Router;
+Backbone.history.start();
